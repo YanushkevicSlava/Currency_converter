ca025ebee0311ecde08a9006e31ee1e59a23ebf4
9Nyw1988 <cool.9nyw@gmail.com>
2023-09-15 12:47:02 +0300

    fix doc

--- app/converter/views.py
+++ app/converter/views.py
@@ -4,9 +4,11 @@ import requests
 
 
 def converter_index(request: HttpRequest) -> HttpResponse:
-    """ Функция конветрации валюты по актуальному курсу"""
+    """ Функция получения курса валют с дальнейшей конвертацией """
 
+    # получение актуального курса валют.
     response = requests.get(url='https://v6.exchangerate-api.com/v6/b251f9d75c48903e2b2cae0d/latest/USD').json()
+
     currencies = response.get("conversion_rates")
 
     if request.method == "GET":
